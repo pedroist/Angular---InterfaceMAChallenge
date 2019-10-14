6b04b56ce71b9ddc661a9a0c83ad0b082bbcaf87
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2019-10-15 00:39:48 +0100

    Added more images. Imported lodash to use random numbers to select pics. Visual improvements

--- src/app/components/houses/house-card/house-card.component.ts
+++ src/app/components/houses/house-card/house-card.component.ts
@@ -1,6 +1,8 @@
 import { House } from './../../../models/House';
 import { Component, OnInit, Input } from '@angular/core';
 
+import * as _ from "lodash";
+
 @Component({
   selector: 'app-house-card',
   templateUrl: './house-card.component.html',
@@ -8,10 +10,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HouseCardComponent implements OnInit {
   @Input() houseCard: House;
+  @Input() index: number;
+
+  imgPath: string = "";
 
   constructor() { }
 
   ngOnInit() {
+    // Pick a random number to select an house image on the assets folder
+    let randIndex = _.random(1, 10);
+    this.imgPath = `../../../../assets/img/house${randIndex}.jpg`;
   }
 
 }
